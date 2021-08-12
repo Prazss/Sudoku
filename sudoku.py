ae8b6f94b47c9cfed956dbeb5b6ada55e5e701a4
Prajnesh Shetty <prazss21@gmail.com>
2021-08-13 01:52:10 +0530

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -8,17 +8,6 @@ sudoku = [[8,3,9,5,7,2,1,4,6],
           [0,0,0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0,0,0]]
 
-"""sudoku = [[0,7,0,0,0,9,0,4,0],
-          [2,0,3,1,4,0,0,0,6],
-          [0,0,0,0,0,0,0,3,0],
-          [8,0,0,0,0,0,0,7,0],
-          [0,1,0,5,6,2,0,9,0],
-          [0,5,0,0,0,0,0,0,4],
-          [0,4,0,0,0,0,0,0,0],
-          [7,0,0,0,2,4,8,0,1],
-          [0,6,0,7,0,0,0,2,0]]"""
-
-
 def printSudoku():
 
     for i in sudoku:
@@ -72,52 +61,3 @@ printSudoku()
 solSudoku(0,0)
 print("\n\nAfter Solution - \n")
 printSudoku()
-
-
-
-"""
-
-
-fixed = [[0,1,0,0,0,1,0,1,0],
-          [1,0,1,1,1,0,0,0,1],
-          [0,0,0,0,0,0,0,1,0],
-          [1,0,0,0,0,0,0,1,0],
-          [0,1,0,1,1,1,0,1,0],
-          [0,1,0,0,0,0,0,0,1],
-          [0,1,0,0,0,0,0,0,0],
-          [1,0,0,0,1,1,1,0,1],
-          [0,1,0,1,0,0,0,1,0]]
-
-def solSudoku(row,column):
-    
-    print(row,column)
-    if fixed[row][column] == 1:
-        if column!=8:
-            solSudoku(row,column+1)
-        elif row!=8:
-            solSudoku(row+1,0)
-        else:
-            return 1
-    else:    
-        for i in range(9):
-            print("Value = "+str(i+1))
-            print("CheckRow = "+str(checkRow(row,column,i+1)))
-            print("CheckCol = "+str(checkColumn(row,column,i+1)))
-            print("CheckCube = "+str(checkCube(row,column,i+1)))
-            if(checkRow(row,column,i+1)==1 and checkColumn(row,column,i+1)==1 and checkCube(row,column,i+1)==1):
-                sudoku[row][column] = i+1
-                print("Again")
-                if row==8 and column==8:
-                    return 1
-                elif column!=8:
-                    return solSudoku(row,column+1)
-                elif row!=8:
-                    return solSudoku(row+1,0)
-            elif(i+1==9):
-                return
-
-
-
-"""
-
-
